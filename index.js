0cd4fd5eb2de81737607a667ff039aa5713176c8
是杜啊 <35400935+duweikang@users.noreply.github.com>
2024-07-30 15:12:14 +0800

    新增函数: 增加yd_is_phone函数

--- index.js
+++ index.js
@@ -54,3 +54,4 @@ export { yd_string_short } from './lib/yd_string_short.js';
 export { yd_string_snakeCase } from './lib/yd_string_snakeCase.js';
 export { yd_tree_array2Tree } from './lib/yd_tree_array2Tree.js';
 export { yd_tree_traverse } from './lib/yd_tree_traverse.js';
+export { yd_is_phone } from './lib/yd_is_phone.js';
