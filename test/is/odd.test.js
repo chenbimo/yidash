5354211c55c62f53fe6e6d2dac36b25d04c7e3b3
Xy2002 <57112423+Xy2002@users.noreply.github.com>
2024-08-01 16:56:16 +0800

    fix: Correct function name in test/is/odd.test.js

--- test/is/odd.test.js
+++ test/is/odd.test.js
@@ -1,24 +1,24 @@
-import { describe, it, expect } from 'vitest';
-import yd_is_odd from '../../lib/is/odd';
+import { describe, expect, it } from 'vitest'
+import yd_is_odd from '../../lib/is/odd'
 
 describe('yd_is_odd', () => {
     it('should be `true`', () => {
-        expect(yd_is_odd(13)).toBeTruthy();
-        expect(yd_is_odd(-13)).toBeTruthy();
-    });
+        expect(yd_is_odd(13)).toBeTruthy()
+        expect(yd_is_odd(-13)).toBeTruthy()
+    })
 
     it('should be `false`', () => {
-        expect(isOdd(0)).toBeFalsy();
-        expect(isOdd(2)).toBeFalsy();
-        expect(isOdd(-2)).toBeFalsy();
-    });
+        expect(yd_is_odd(0)).toBeFalsy()
+        expect(yd_is_odd(2)).toBeFalsy()
+        expect(yd_is_odd(-2)).toBeFalsy()
+    })
 
     it('should throw Error', () => {
-        expect(() => isOdd(true)).toThrowError('value must be a number');
-        expect(() => isOdd(Symbol())).toThrowError('value must be a number');
-        expect(() => isOdd([])).toThrowError('value must be a number');
-        expect(() => isOdd({})).toThrowError('value must be a number');
-        expect(() => isOdd(new Date())).toThrowError('value must be a number');
+        expect(() => yd_is_odd(true)).toThrowError('value must be a number')
+        expect(() => yd_is_odd(Symbol(''))).toThrowError('value must be a number')
+        expect(() => yd_is_odd([])).toThrowError('value must be a number')
+        expect(() => yd_is_odd({})).toThrowError('value must be a number')
+        expect(() => yd_is_odd(new Date())).toThrowError('value must be a number')
         // ...
-    });
-});
+    })
+})
