4dfd39007a3973f8af830cca4498dd57ca6fb833
grantguo <810153274@qq.com>
2024-08-01 20:36:55 +0800

    冲突问题

--- test/browser/denyChange.test.js
+++ test/browser/denyChange.test.js
@@ -1,8 +1,8 @@
-// import { describe, it, expect } from 'vitest';
-// import yd_browser_denyChange from '../../lib/browser/denyChange.js';
+import { describe, it, expect } from 'vitest';
+import yd_browser_denyChange from '../../lib/browser/denyChange.js';
 
-// describe('yd_browser_denyChange', () => {
-//     it('默认单测', () => {
-//         expect(true).toBe(true);
-//     });
-// });
+describe('yd_browser_denyChange', () => {
+    it('默认单测', () => {
+        expect(true).toBe(true);
+    });
+});
