5ded8cb877e04fa1974683f705f69daa21b5143c
Shu Yi Kang <185901261@qq.com>
2024-08-02 18:25:00 +0800

    新增函数: 增加yd_url_parse函数

---
+++ test/url/parse.test.js
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest';
+import yd_url_parse from '../../lib/url/parse.js';
+describe('yd_url_parse', () => {
+    it('yd_url_parse测试', () => {
+        expect(yd_url_parse('https://example.com/api/data?page=57&limit=28&sort=desc&keyword=random_abc123')).toStrictEqual({
+            page: '57',
+            limit: '28',
+            sort: 'desc',
+            keyword: 'random_abc123'
+        });
+    });
+});
