a4a55e546c523569c75674447cd019c964075664
陈随易 <chenbimo@gmail.com>
2024-08-02 17:15:17 +0800

    思考计划: 去除testPend目录

--- testPend/faker/generateChineseLorem.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import yd_faker_generateChineseLorem from '../../lib/faker/generateChineseLorem.js';
-
-describe('yd_faker_generateChineseLorem', () => {
-    it('默认单测', () => {
-        expect(true).toBe(true);
-    });
-});
--- testPend/faker/generateSentenceLorem.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import yd_faker_generateSentenceLorem from '../../lib/faker/generateSentenceLorem.js';
-
-describe('yd_faker_generateSentenceLorem', () => {
-    it('默认单测', () => {
-        expect(true).toBe(true);
-    });
-});
