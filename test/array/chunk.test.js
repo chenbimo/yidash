166abb66fc9c28941a5b2a5e924e889c354835d2
grantguo <810153274@qq.com>
2024-08-02 21:16:11 +0800

    yd_array_chunk 增加剩余元素是否组成一个区块

--- test/array/chunk.test.js
+++ test/array/chunk.test.js
@@ -3,11 +3,14 @@ import yd_array_chunk from '../../lib/array/chunk.js';
 
 describe('yd_array_chunk', () => {
     it('默认单测', () => {
-        const arr = ['a', 'b', 'c', 'd'];
+        const arr = ['a', 'b', 'c', 'd', 'e'];
         const new_arr = yd_array_chunk(arr, 3);
-        console.log(new_arr);
+        expect(new_arr).toEqual([
+            ['a', 'b', 'c'],
+            ['d', 'e']
+        ]);
         const new_arr1 = yd_array_chunk(arr, 3, false);
-        console.log(new_arr1);
+        expect(new_arr1).toEqual([['a', 'b', 'c']]);
         expect(true).toBe(true);
     });
 });
