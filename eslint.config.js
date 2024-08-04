6a0c13986cf9284f50e163c4be6c5ee26c0653ca
陈随易 <chenbimo@gmail.com>
2024-08-04 09:45:29 +0800

    调整配置: 增加jsdoc的eslint配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import jsdoc from 'eslint-plugin-jsdoc';
 
 export default [
     {
@@ -14,12 +15,47 @@ export default [
     },
     pluginJs.configs.recommended,
     eslintPluginPrettierRecommended,
+    jsdoc.configs['flat/recommended'],
     {
         rules: {
             'no-prototype-builtins': 'off',
             'max-len': 'off',
             'no-unused-vars': 'off',
-            'linebreak-style': ['error', 'unix']
+            'linebreak-style': ['error', 'unix'],
+            'jsdoc/require-example': 1,
+            'jsdoc/check-tag-names': [
+                'error',
+                {
+                    definedTags: ['category']
+                }
+            ],
+            'jsdoc/sort-tags': [
+                'error',
+                {
+                    linesBetween: 1,
+                    tagSequence: [
+                        {
+                            tags: [
+                                //
+                                'alias',
+                                'category',
+                                'params',
+                                'return',
+                                'returns',
+                                'version',
+                                'deprecated',
+                                'since',
+                                'author',
+                                'summary',
+                                'describe',
+                                'todo',
+                                'example',
+                                '-other'
+                            ]
+                        }
+                    ]
+                }
+            ]
         }
     }
 ];
